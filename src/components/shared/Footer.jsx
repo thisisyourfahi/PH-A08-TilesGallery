c3579400d616e825f88c114aec25f2d23b41953b
thisisyourfahi <fahiyanofficial@gmail.com>
2026-05-13 02:30:58 +0600

    style: w-2/3

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+        <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10 mt-20">
             <nav className="grid grid-flow-col gap-4">
                 <Link href={'/'} className="link link-hover">Home</Link>
                 <Link href={'/all-tiles'} className="link link-hover">All Tiles</Link>
