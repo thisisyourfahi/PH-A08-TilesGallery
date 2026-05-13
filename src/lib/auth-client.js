23c9e5ba774f81f9f9a8d013b000165f03470c14
thisisyourfahi <fahiyanofficial@gmail.com>
2026-05-13 16:08:31 +0600

    removed /

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://ph-a08-tiles-gallery.vercel.app/"
+    baseURL: "https://ph-a08-tiles-gallery.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
