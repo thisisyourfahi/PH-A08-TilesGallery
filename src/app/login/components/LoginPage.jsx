d788270133e185927449403da57eaea0f559ea51
thisisyourfahi <fahiyanofficial@gmail.com>
2026-05-14 00:48:14 +0600

    style: fix form size

--- src/app/login/components/LoginPage.jsx
+++ src/app/login/components/LoginPage.jsx
@@ -48,10 +48,10 @@ const LoginPage = () => {
     }
     return (
         <div className='animate__animated animate__bounceInUp'>
-            <Card onSubmit={onSubmit} className="border border-gray-200 mx-auto md:w-125 py-10 mt-5">
+            <Card onSubmit={onSubmit} className="border border-gray-200 mx-auto py-10 mt-5">
                 <h1 className="text-center text-2xl font-bold">Log In</h1>
 
-                <Form className="flex w-96 mx-auto flex-col gap-4 pr-4">
+                <Form className="flex w-86 md:w-96 mx-auto flex-col gap-4 pr-4">
                     <TextField
                         isRequired
                         name="email"
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -30,10 +30,10 @@ const RegisterPage = () => {
     }
     return (
         <div className='animate__animated animate__bounceInUp'>
-            <Card className="border border-gray-200 mx-auto md:w-125 py-10 mt-5">
+            <Card className="border border-gray-200 mx-auto py-10 mt-5">
                 <h1 className="text-center text-2xl font-bold">Register</h1>
 
-                <Form onSubmit={onSubmit} className="flex w-96 mx-auto flex-col gap-4 pr-4">
+                <Form onSubmit={onSubmit} className="flex w-86 md:w-96 mx-auto flex-col gap-4">
                     <TextField isRequired name="name" type="text">
                         <Label>Name</Label>
                         <Input placeholder="Enter your name" />
